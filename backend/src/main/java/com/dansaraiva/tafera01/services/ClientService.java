935013abebae9b2379366448583f3bb4d59ea5a3
Daniel Saraiva <saraivadaniel@protonmail.com>
2021-03-24 19:05:02 -0300

    Delete Client

--- backend/src/main/java/com/dansaraiva/tafera01/services/ClientService.java
+++ backend/src/main/java/com/dansaraiva/tafera01/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dansaraiva.tafera01.dto.ClientDTO;
 import com.dansaraiva.tafera01.entities.Client;
 import com.dansaraiva.tafera01.repositories.ClientRepository;
+import com.dansaraiva.tafera01.services.exceptions.DatabaseException;
 import com.dansaraiva.tafera01.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -61,6 +64,19 @@ public class ClientService {
 			throw new ResourceNotFoundException("Id not found");
 		}
 
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 
 }
