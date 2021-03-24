f3028e1fd703bcc779aecba39d29db3ff0ec002a
Daniel Saraiva <saraivadaniel@protonmail.com>
2021-03-24 18:43:44 -0300

    Update Client

--- backend/src/main/java/com/dansaraiva/tafera01/services/ClientService.java
+++ backend/src/main/java/com/dansaraiva/tafera01/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,18 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dansaraiva.tafera01.dto.ClientDTO;
 import com.dansaraiva.tafera01.entities.Client;
 import com.dansaraiva.tafera01.repositories.ClientRepository;
-import com.dansaraiva.tafera01.services.exceptions.EntityNotFoundException;
+import com.dansaraiva.tafera01.services.exceptions.ResourceNotFoundException;
 
-@Service 
+@Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll(){
-		List<Client> list = repository.findAll();		
-		return list.stream().map(cli -> new ClientDTO(cli)).collect(Collectors.toList());		
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(cli -> new ClientDTO(cli)).collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
@@ -31,7 +33,7 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
-	
+
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
@@ -41,8 +43,24 @@ public class ClientService {
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
 		entity = repository.save(entity);
-		return new ClientDTO(entity);		
+		return new ClientDTO(entity);
 	}
-	
-	
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found");
+		}
+
+	}
+
 }
